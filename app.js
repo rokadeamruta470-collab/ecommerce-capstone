cb67178964b39cefc2dfdd2fd76ce9ae0ae4dae3
Amruta Rokade <rokadeamruta470@gmail.com>
2026-09-19 13:25:00 -0700

    Add javascript functionality

---
+++ app.js
@@ -0,0 +1,10 @@
+const productContainer = document.getElementById("productContainer");
+
+productContainer.innerHTML = `
+    <div class="product-card">
+        <h3>Test Product</h3>
+        <p>JavaScript is working successfully.</p>
+        <p class="price">$99</p>
+        <button>Add to Cart</button>
+    </div>
+`;
